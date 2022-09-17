fd87c9c81a2ede0ddae4a22d28deea40bd36f59e
kellydhunter1 <kellydhunter1@gmail.com>
2022-09-16 22:23:26 -0400

    added header background and removed headshot

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,33 @@
-import headshot from '../../assets/img/headshot.jpg';
-import ReactDom from 'react';
+// import headshot from '../../assets/img/headshot.jpg';
+import '../../index.css';
+
 
 function Header() {
     return (
-
-        <div>
-
-<div class="px-4 py-3 my-5 text-center">
-<img className="img-thumbnail" src={ headshot } alt="Kelly Hunter" width="25%" />
-<h1 class="display-5 fw-bold">Kelly Hunter</h1>
-<div class="col-lg-6 mx-auto">
-  <h2 class="lead mb-4">MERN Stack Web Developer</h2>
+      <div>
+{/* background, text and picture */}
+      <div className="px-4 py-3 my-5 text-center" id="header">
+      {/* <img className="img-thumbnail" src={ headshot } alt="Kelly Hunter" width="25%" /> */}
+      <h1 class="display-5 text-white fw-bold">Kelly Hunter</h1>
+      <div class="col-lg-6 mx-auto">
+      <h2 class="lead mb-4">MERN Stack Web Developer</h2>
 {/* buttons */}
-  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
     
-    {/* linkedin button */}
+{/* linkedin button */}
     <a target="_blank"
     rel="noreferrer"
     href="https://www.linkedin.com/in/kelly-d-hunter" type="submit" class="btn btn-primary btn-lg px-4 gap-3">LinkedIn</a>
     
-    {/* github button */}
+{/* github button */}
     <a 
     target="_blank"
     rel="noreferrer"
     href="https://www.github.com/kellydhunter1" type="submit" class="btn btn-outline-secondary btn-lg px-4">GitHub</a> 
-  </div>
-</div>
-</div> 
-        </div>
+    </div>
+    </div>
+    </div> 
+    </div>
     )
 };
 
